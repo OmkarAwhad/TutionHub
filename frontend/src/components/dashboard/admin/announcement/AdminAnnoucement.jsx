244c406919702a5d38c52b2ead0386f58ebfc5dd
OmkarAwhad <omkarawhad3007@gmail.com>
2025-07-28 16:41:00 +0530

    some updates

--- frontend/src/components/dashboard/admin/announcement/AdminAnnoucement.jsx
+++ frontend/src/components/dashboard/admin/announcement/AdminAnnoucement.jsx
@@ -5,7 +5,7 @@ function AdminAnnouncement() {
 	return (
 		<div className="w-full min-h-[70vh] flex items-center justify-center p-3 sm:p-4 lg:p-6">
 			{/* button container */}
-			<div className="w-full max-w-4xl flex flex-col sm:flex-row sm:flex-wrap items-center justify-center gap-6 sm:gap-10 lg:gap-20">
+			<div className="w-full max-w-4xl flex flex-col md:flex-row items-center justify-center gap-6 sm:gap-10 lg:gap-20">
 				<Link
 					to="/dashboard/admin-announcement/create-announcement"
 					className="w-full sm:w-[48%] bg-medium-gray px-6 sm:px-10 lg:px-20 py-10 sm:py-12 lg:py-14 text-white font-extrabold text-2xl sm:text-3xl text-center rounded-lg hover:bg-charcoal-gray hover:scale-[102%] transition-all duration-200"
